4cb1641ffbaf04b377ae0151c5e5d5d22269502e
elyasafzider <44776595+elyasafzider@users.noreply.github.com>
2019-05-30 16:23:28 +0300

    Add files via upload

---
+++ pipes.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/fcntl.h>
